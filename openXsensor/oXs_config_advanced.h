8eea56468547359cd128fe2106b69ba33949e978
rottweilertje <rottweilertje@rottnic.nl>
2021-09-06 22:40:18 +0200

    Change speed to kph instead of knots

--- openXsensor/oXs_config_advanced.h
+++ openXsensor/oXs_config_advanced.h
@@ -170,7 +170,7 @@
 //#define PIN_PUSHBUTTON    2   // default is 10 but my own device is 2
 
 // --------- 9 - GPS ------------------------------------------------------------------------------------------------
-//#define GPS_SPEED_IN_KMH  // uncomment this line if GPS speed has to be sent in km/h instead of knot/h (only for Frsky protocol)
+#define GPS_SPEED_IN_KMH  // uncomment this line if GPS speed has to be sent in km/h instead of knot/h (only for Frsky protocol)
 #define GPS_SPEED_3D      // uncomment this line if GPS speed has to be the 3d speed instead of the 2d speed (note: 3d is probably less accurate - to test) 
 #define GPS_REFRESH_RATE  5      // rate at which GPS sent new data; select between 1, 5 or 10 (Hz). Default = 5 Hz; Ublox NEO6 does not support 10 hz  
 
